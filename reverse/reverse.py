f96dd44e0775cd3b40c13afb21823d2ad091f7c3
VanshikaP <vanshikapundir92@gmail.com>
2020-07-17 12:50:38 -0700

    recursive solution for reverse

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -39,20 +39,42 @@ class LinkedList:
 
         return False
     
+    def remove_from_tail(self):
+        if self.head is None:
+            return None
+        if self.head.next_node is None:
+            self.head = None
+            self.tail = None
+        else:
+            currentNode = self.head
+            while currentNode.next_node.next_node is not None:
+                currentNode = currentNode.next_node
+
+            new_tail_node = currentNode
+            tail_node = currentNode.next_node
+            new_tail_node.next_node = None
+            return tail_node.value
+    
+
     def reverse_list(self, node, prev):
         if self.head is None:
             return
         
-        if self.head.next_node is None:
+        elif self.head.next_node is None:
             return
+        # base case - 2 elements
+        elif self.head.next_node.next_node is None:
+            head_node = self.head
+            tail_node = self.head.next_node
+            self.head = tail_node
+            self.head.next_node = head_node
+            self.head.next_node.next_node = None
+        else:
+            tail_value = self.remove_from_tail()
+            self.reverse_list(self.head, None)
+            self.add_to_head(tail_value)
         
-        currentNode = node
-        while currentNode is not None:
-            next = currentNode.next_node
-            currentNode.next_node = prev
-            prev = currentNode
-            currentNode = next
-        self.head = prev
+        
 
     
 
