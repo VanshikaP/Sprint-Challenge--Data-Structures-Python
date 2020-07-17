38cbb8afe8b740b688a4a14b5303bc8e5116d422
VanshikaP <vanshikapundir92@gmail.com>
2020-07-17 08:25:10 -0700

    names solution

--- names/names.py
+++ names/names.py
@@ -18,6 +18,41 @@ for name_1 in names_1:
         if name_1 == name_2:
             duplicates.append(name_1)
 
+# Sol1 - 1.46s - O(n^2)
+# for name_1 in names_1:
+    # if name_1 in names_2:
+    # duplicates.append(name_1)
+
+# Sol2 - 1.6s - O(n^2)
+# duplicates = [name for name in names_1 if name in names_2]
+
+
+# Sol3 - sort and then binary search - O(nlog(n)) - 0.5s
+# Binary Search - log(n)
+def myBinarySearch(items, item):
+    if len(items) is 1:
+        if items[0] is item:
+            return True
+        else:
+            return False
+    elif item == items[int(len(items)/2)]:
+        return True
+    elif item < items[int(len(items)/2)]:
+        newItems = items[0:int(len(items)/2)]
+        return myBinarySearch(newItems, item)
+    else:
+        newItems = items[int(len(items)/2):]
+        return myBinarySearch(newItems, item)
+
+# Built in Sorting - nlog(n)
+names_1.sort() 
+
+# Iterating over array and searching - nlog(n)
+for name in names_2:
+    if myBinarySearch(names_1, name):
+        duplicates.append(name)
+
+
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
